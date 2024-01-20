7b48761cb4d05cccb81edea70c50f45efea79643
Alysha Huot <alyshajhuot@gmail.com>
2024-01-20 09:30:03 -0700

    change getAllProperties function to connect to psql database

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,5 +1,14 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
+const { Pool } = require('pg');
+
+const pool = new Pool ({
+  user: 'jer',
+  host: 'localhost',
+  database: 'lightbnb',
+  password: 'blah1234',
+  port: 5432
+});
 
 /// Users
 
@@ -60,12 +69,22 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+
+  return pool
+    .query(
+      `SELECT * FROM properties
+      LIMIT $1`,
+      [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
 };
+
 
 /**
  * Add a property to the database
